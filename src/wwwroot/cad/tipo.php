2ac6a1d8561fe9a85c2e502073316e0c94d27603
Luiz Neto <lu_u_z@hotmail.com>
2016-10-04 20:38:58 -0300

    v127.0.1.1
    CONSULTE O ARQUIVO README

--- src/wwwroot/cad/tipo.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include ("/php/conecao.php");
-	$query ="SELECT id,tipoveiculo FROM tipoveiculo";
-	$check = mysql_query($query, $dbc);
-	while ($dados = mysql_fetch_array($check)) 
-	{
-		echo("<option value='".$dados['id']."'>".$dados['tipoveiculo']."</option>");
-	}
-?>
